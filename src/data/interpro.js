2b82932e4a8d7b95141c7b3e71c463620f0e1f70
Siddhartha Basu <biosidd@gmail.com>
2015-06-26 22:52:49 +0100

    test data file for interpro domain visualization

---
+++ src/data/interpro.js
@@ -0,0 +1,169 @@
+let data = {
+    "domains": {
+        "G3DSA:1.20.5.340": [
+            {
+                "analysis": "GENE3D",
+                "end": "902",
+                "score": "1.0E-4",
+                "start": "819",
+                "status": "T"
+            }
+        ],
+        "PF00063": [
+            {
+                "analysis": "PFAM",
+                "end": "747",
+                "interpro_acc": "IPR001609",
+                "score": "0.0",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "88",
+                "status": "T"
+            }
+        ],
+        "PF01576": [
+            {
+                "analysis": "PFAM",
+                "end": "1311",
+                "interpro_acc": "IPR002928",
+                "score": "3.7999999999999986E-20",
+                "interpro_desc": "Myosin tail",
+                "start": "1049",
+                "status": "T"
+            },
+            {
+                "analysis": "PFAM",
+                "end": "1495",
+                "interpro_acc": "IPR002928",
+                "score": "4.299999999999985E-18",
+                "interpro_desc": "Myosin tail",
+                "start": "1258",
+                "status": "T"
+            },
+            {
+                "analysis": "PFAM",
+                "end": "2085",
+                "interpro_acc": "IPR002928",
+                "score": "1.2000000000000005E-51",
+                "interpro_desc": "Myosin tail",
+                "start": "1480",
+                "status": "T"
+            }
+        ],
+        "PR00193": [
+            {
+                "analysis": "PRINTS",
+                "end": "135",
+                "interpro_acc": "IPR001609",
+                "score": "1.1000015067164284E-60",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "116",
+                "status": "T"
+            },
+            {
+                "analysis": "PRINTS",
+                "end": "197",
+                "interpro_acc": "IPR001609",
+                "score": "1.1000015067164284E-60",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "172",
+                "status": "T"
+            },
+            {
+                "analysis": "PRINTS",
+                "end": "247",
+                "interpro_acc": "IPR001609",
+                "score": "1.1000015067164284E-60",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "220",
+                "status": "T"
+            },
+            {
+                "analysis": "PRINTS",
+                "end": "477",
+                "interpro_acc": "IPR001609",
+                "score": "1.1000015067164284E-60",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "449",
+                "status": "T"
+            },
+            {
+                "analysis": "PRINTS",
+                "end": "531",
+                "interpro_acc": "IPR001609",
+                "score": "1.1000015067164284E-60",
+                "interpro_desc": "Myosin head, motor domain",
+                "start": "503",
+                "status": "T"
+            }
+        ],
+        "PS50096": [
+            {
+                "analysis": "PROFILE",
+                "end": "791",
+                "interpro_acc": "IPR000048",
+                "score": "0.0",
+                "interpro_desc": "IQ motif, EF-hand binding site",
+                "start": "762",
+                "status": "T"
+            }
+        ],
+        "PTHR13140": [
+            {
+                "analysis": "PANTHER",
+                "end": "2109",
+                "score": "0.0",
+                "start": "29",
+                "status": "T"
+            }
+        ],
+        "SSF90257": [
+            {
+                "analysis": "SSF",
+                "end": "1050",
+                "score": "7.849999999182048E-11",
+                "start": "941",
+                "status": "T"
+            },
+            {
+                "analysis": "SSF",
+                "end": "1132",
+                "score": "2.8800000000051007E-5",
+                "start": "1060",
+                "status": "T"
+            },
+            {
+                "analysis": "SSF",
+                "end": "1246",
+                "score": "6.5399999996345615E-12",
+                "start": "1131",
+                "status": "T"
+            },
+            {
+                "analysis": "SSF",
+                "end": "1358",
+                "score": "1.4899999999921981E-6",
+                "start": "1250",
+                "status": "T"
+            },
+            {
+                "analysis": "SSF",
+                "end": "1442",
+                "score": "1.7500000000149317E-8",
+                "start": "1334",
+                "status": "T"
+            },
+            {
+                "analysis": "SSF",
+                "end": "1661",
+                "score": "1.0700000000118023E-5",
+                "start": "1549",
+                "status": "T"
+            }
+        ]
+    },
+    "id": "P08799",
+    "length": "2116",
+    "total_analysis": 7
+};
+
+export { data };
