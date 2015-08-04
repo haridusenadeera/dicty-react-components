0de1cc3d7572efd4ab2cdd54ac7cd7845bc2e689
Siddhartha Basu <biosidd@gmail.com>
2015-08-04 17:36:50 +0000

    removed css loader added babel loader

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,12 +4,10 @@
  */
 
 'use strict';
-var webpack = require('webpack');
 var fs = require('fs');
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 var examples_dir = path.resolve(__dirname);
+
 function buildEntries(dir) {
     var entries = {}
     fs.readdirSync(dir).forEach(function(entry) {
@@ -23,41 +21,25 @@ function buildEntries(dir) {
 }
 
 module.exports = {
+  devtool: 'source-map',
+  cache: true,
+  entry: buildEntries(examples_dir),
   output: {
     filename: '[name].js',
     path: './build'
   },
-  entry: buildEntries(examples_dir),
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css'],
+    extensions: ['', '.js', '.jsx'],
     alias: {
-        jquery: 'jquery/dist/jquery.js',
         'dicty-react-components': '../../src/index'
     }
   },
   module: {
         loaders: [{
-          test: /\.jsx$/,
-          loader: 'jsx-loader?harmony'
-        }, {
-          test: /\.css$/,
-          loader: 'style-loader!css-loader'
-        }, {
-          test: /\.(png|jpg)$/,
-          loader: 'url-loader?limit=8192'
-        },
-        { test: /\.woff$/,   loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-        { test: /\.ttf$/,    loader: "file-loader" },
-        { test: /\.eot$/,    loader: "file-loader" },
-        { test: /\.svg$/,    loader: "file-loader" }
-    ]
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin(
-        {
-            jQuery: "jquery"
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel?stage=0'
         }
-    )
-  ]
+    ]
+  }
 };
