32fb0d877a90f5302f9b0140dc5498bd9f71d9e0
Siddhartha Basu <biosidd@gmail.com>
2015-08-04 21:22:28 +0000

    added a script to generate demos from examples

---
+++ generate_demo.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/zsh
+
+export PATH=${PWD}/node_modules/.bin:$PATH
+cd examples && webpack --progress
+cd ..
+mkdir -p guides/demo
+cp examples/*.html guides/demo/
+cp -R examples/build guides/demo/
